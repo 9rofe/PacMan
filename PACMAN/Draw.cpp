494bcac60b07d93f5bb9c3eb4add2c3d633e6f91
PaceyMcGoo <paceymcgarry@gmail.com>
2022-12-03 00:13:20 -0800

    Made Sprite and applied Texture | Pacey

--- PACMAN/Draw.cpp
+++ PACMAN/Draw.cpp
@@ -28,15 +28,20 @@ void Engine::Draw()
     // 
     //Draws "Player"
     RectangleShape d_player(Vector2f(40, 40));
+    Sprite dd_player;
+    dd_player.setPosition(d_player.getPosition());
+    Texture text;
+    text.loadFromFile("Textures/BluePacmanGhost.png");
+    dd_player.setTexture(text);
     
     if (m_Player->GetLocation().y <= 0.0 || m_Player->GetLocation().y >= 1080.0)
     {
         m_Player->SetLocation(m_Player->GetLocation());
         //playerDirection = Direction::NONE;
     }
-    d_player.setPosition(m_Player->GetLocation());
+    dd_player.setPosition(m_Player->GetLocation());
     
-    m_Window.draw(d_player);
+    m_Window.draw(dd_player);
     //m_Window.draw();
 	m_Window.draw(feed);
 	m_Window.display();
