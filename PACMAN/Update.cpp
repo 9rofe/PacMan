03fb977a8357b72702ab34d7750ad217fe8c9822
PaceyMcGoo <paceymcgarry@gmail.com>
2022-12-04 20:46:40 -0800

    Refined some stuff | Pacey

--- PACMAN/Update.cpp
+++ PACMAN/Update.cpp
@@ -48,7 +48,7 @@ void Engine::Update()
 	//update m_dotsMap and m_fruitMap
 	
 	//PLAYER RECT
-	Rect<float> playerRect(m_Player->GetLocation(), { 80.0, 80.0 });
+	Rect<float> playerRect(m_Player->GetLocation(), { 60.0, 60.0 });
 	for (unsigned int counter = 0; counter < m_wallsMap.size(); counter++)
 	{
 		bool collidesWalls = m_wallsMap.at(counter).intersects(playerRect);
@@ -63,33 +63,31 @@ void Engine::Update()
 		{
 			m_Player->SetLocation(Vector2f(m_Player->GetLocation().x - 10.0f, m_Player->GetLocation().y));
 			cout << "WALL COLLISION! RIGHT" << endl;
-			//playerDirection = prev_Direction;
 			playerDirection = Direction::NONE;
 		}
 		else if (collidesWalls && playerDirection == Direction::UP)
 		{
 			m_Player->SetLocation(Vector2f(m_Player->GetLocation().x, m_Player->GetLocation().y + 10.0f));
 			cout << "WALL COLLISION! UP" << endl;
-			//playerDirection = prev_Direction;
 			playerDirection = Direction::NONE;
 		}
 		else if (collidesWalls && playerDirection == Direction::DOWN)
 		{
 			m_Player->SetLocation(Vector2f(m_Player->GetLocation().x, m_Player->GetLocation().y - 10.0f));
 			cout << "WALL COLLISION! DOWN" << endl;
-			//playerDirection = prev_Direction;
 			playerDirection = Direction::NONE;
 		}
 	}
 		//enemy collision
 		for (unsigned int counter = 0; counter < m_Enemies.size(); counter++)
 		{
-			Rect<float> enemyRect(m_Enemies.at(counter).GetLocation(), {80.0, 80.0});
+			Rect<float> enemyRect(m_Enemies.at(counter).GetLocation(), {60.0, 60.0});
 			bool collidesEnemy = enemyRect.intersects(playerRect);
-			if (collidesEnemy)// && m_Enemies.at(counter).getDirection() == Direction::UP)
+			if (collidesEnemy)
 			{
 				m_Player->SetLives(-1);
-				m_Player->SetLocation(Vector2f(resolution.x / 2.0f, resolution.y / 1.1f));
+				m_Player->SetLocation(Vector2f(resolution.x / 3.3f, resolution.y / 1.1f));
+				playerDirection = Direction::RIGHT;
 				cout << "ENEMY COLLISION!" << "LIVES: " << m_Player->GetLives() << endl;
 				if (m_Player->GetLives() <= 0)
 				{
@@ -98,10 +96,12 @@ void Engine::Update()
 				}
 				if (m_Enemies.at(counter).getDirection() == Direction::DOWN)
 				{
+					m_Enemies.at(counter).SetLocation(Vector2f(m_Enemies.at(counter).GetLocation().x, m_Enemies.at(counter).GetLocation().y + 10.0f));
 					m_Enemies.at(counter).SetDirection(Direction::UP);
 				}
 				else
 				{
+					m_Enemies.at(counter).SetLocation(Vector2f(m_Enemies.at(counter).GetLocation().x, m_Enemies.at(counter).GetLocation().y-10.0f));
 					m_Enemies.at(counter).SetDirection(Direction::DOWN);
 				}
 			}
@@ -115,16 +115,11 @@ void Engine::Update()
 			dotsIter = m_dotsMap.erase(dotsIter);
 			currentScore += 1;
 		}
-		else if(m_dotsMap.size() == 0 && m_fruitMap.size() == 0)
-		{
-			//reset
-		}
 		else
 		{
 			++dotsIter;
 		}
 	}
-
 	while (fruitIter != m_fruitMap.end()) {
 		if (fruitIter->intersects(playerRect))
 		{
@@ -136,5 +131,9 @@ void Engine::Update()
 		{
 			++fruitIter;
 		}
+	}
+	if (m_dotsMap.size() == 0 && m_fruitMap.size() == 0)
+	{
+		//reset
 	}
 }
